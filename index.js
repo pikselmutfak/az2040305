a7a0ba8b6e357524dff2442081332dbd0c90baab
Kazım Etiksan <kazim@pikselmutfak.com>
2021-05-04 13:45:09 +0300

    index.js updated

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/api/hello', (req, res) => {
 
 app.get('/api/merhaba', (req, res) => {
 
-    res.send('Merhaba Dünya')
+    res.send('Merhaba AZ204')
 })
 
 app.post('/api/name', (req, res) => {
